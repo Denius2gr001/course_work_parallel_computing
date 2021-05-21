b74aeda49741666762410c3dea9b88f94e27f6bd
Denius2gr001 <Anzh29@gmail.com>
2021-05-22 02:17:58 +0300

    add!: Multi-thread mode building added

--- src/com/coursework/InvertedIndex.java
+++ src/com/coursework/InvertedIndex.java
@@ -1,6 +1,8 @@
 package com.coursework;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +19,44 @@ public class InvertedIndex {
     }
 
     private static Map<String, List<String>> parallelBuild(List<File> files, int threadsNum) {
-        return null;
+        Map<String, List<String>> invertedIndex = new HashMap<>();
+
+        BuildThread[] threads = new BuildThread[threadsNum];
+        for (int threadId = 0; threadId < threadsNum; threadId++) {
+            int startPosition = files.size() / threadsNum * threadId;
+            int endPosition = files.size() / threadsNum * (threadId + 1);
+
+            threads[threadId] = new BuildThread(files.subList(startPosition, endPosition), threadId);
+            threads[threadId].start();
+        }
+
+        try {
+            for (BuildThread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        for (BuildThread thread : threads) {
+            addNewPartToIndex(invertedIndex, thread.getInvertedIndexPart());
+        }
+
+        return invertedIndex;
+    }
+
+    private static void addNewPartToIndex(Map<String, List<String>> index, Map<String, List<String>> newPart) {
+        for (Map.Entry<String, List<String>> item : newPart.entrySet()) {
+            for (String position : item.getValue()) {
+                if (index.containsKey(item.getKey())) {
+                    index.get(item.getKey()).add(position);
+                } else {
+                    List<String> positionsList = new LinkedList<>();
+                    positionsList.add(position);
+
+                    index.put(item.getKey(), positionsList);
+                }
+            }
+        }
     }
 }

f7baae8bc4d9beb100e25cf00c2289c01b4b95d2
Denius2gr001 <Anzh29@gmail.com>
2021-05-22 02:37:25 +0300

    add: Writing index to the file enabled

--- src/com/coursework/UserInputHandler.java
+++ src/com/coursework/UserInputHandler.java
@@ -1,11 +1,16 @@
 package com.coursework;
 
-import static java.lang.Integer.parseInt;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import java.util.Scanner;
+import static java.lang.Integer.parseInt;
 
 public class UserInputHandler {
     private static final Scanner IN = new Scanner(System.in);
+    private static final Calendar CALENDAR = new GregorianCalendar();
     private static final int EXIT_CODE = -1;
     private static final int MIN_THREADS_NUM = 1;
     private static final int MAX_THREADS_NUM = 1;
@@ -33,5 +38,45 @@ public class UserInputHandler {
 
             System.out.println("Incorrect symbol! Should be the number (from 1 to 50)");
         }
+    }
+
+    public static boolean isWritingToTheFileRequired() {
+        System.out.println("Write built index to the file? [Yes/No, Y/N]: ");
+        String answer = IN.nextLine();
+
+        return answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("y");
+    }
+
+    public static boolean createInvertedIndexFile(Map<String, List<String>> invertedIndex, int threadsNum) {
+        String fileName = "IndexFile_" + threadsNum + "threads_" +
+                CALENDAR.get(Calendar.YEAR) +
+                CALENDAR.get(Calendar.MONTH) +
+                CALENDAR.get(Calendar.DATE) +
+                CALENDAR.get(Calendar.MINUTE) +
+                CALENDAR.get(Calendar.SECOND) +
+                ".txt";
+
+        List<String> dictionary = new ArrayList<>(invertedIndex.keySet()).stream()
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            for (String word : dictionary) {
+                StringBuilder indexItem = new StringBuilder(word + " -> ");
+                for (String position : invertedIndex.get(word)) {
+                    indexItem.append(position)
+                            .append("  ");
+                }
+                indexItem.append("\n");
+
+                writer.write(indexItem.toString());
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error while writing the inverted index to the file!");
+            return false;
+        }
+
+        return true;
     }
 }

924e928e40151323c504482f7a2aa3e43723b564
Denius2gr001 <Anzh29@gmail.com>
2021-05-22 02:49:07 +0300

    add: Main class implemented

--- src/com/coursework/Main.java
+++ src/com/coursework/Main.java
@@ -1,8 +1,40 @@
 package com.coursework;
 
+import java.io.File;
+import java.util.*;
+
 public class Main {
+    private final static String SOURCE = "source";
+    private final static int EXIT_CODE = -1;
 
     public static void main(String[] args) {
-	// write your code here
+        File sourceFolder = new File(SOURCE);
+
+        List<File> files = new ArrayList<>();
+        if (sourceFolder.isDirectory() && Objects.requireNonNull(sourceFolder.listFiles()).length > 0) {
+            files = Arrays.asList(Objects.requireNonNull(sourceFolder.listFiles()));
+        }
+
+        if (files.size() == 0) {
+            System.out.println("The source directory is empty! Nothing to process!");
+            return;
+        }
+
+        int threadsNum = UserInputHandler.getThreadsNum();
+        if (threadsNum == EXIT_CODE) {
+            System.out.println("Exiting...");
+            return;
+        }
+
+        long startMoment = System.currentTimeMillis();
+        Map<String, List<String>> invertedIndex = InvertedIndex.build(files, threadsNum);
+        long finishMoment = System.currentTimeMillis();
+
+        System.out.println("The inverted index built for " + (finishMoment - startMoment) + "ms" );
+        System.out.println("(Index size: " + invertedIndex.size() + " items)");
+
+        if (UserInputHandler.isWritingToTheFileRequired()) {
+            UserInputHandler.createInvertedIndexFile(invertedIndex, threadsNum);
+        }
     }
 }
